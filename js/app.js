4b019115af5df21725d068480807b151900ad162
Aaron Reisman <aaron@lifeiscontent.net>
2012-04-06 01:07:01 -0700

    Added Users Model/Collection

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-var firebase, Message, message, MessageList, messages;
+var firebase, Message, Messages, messages, User, Users, users;
 firebase = new Firebase('http://gamma.firebase.com/populist/');
 Message = Backbone.Model.extend({});
-MessageList = Backbone.Collection.extend({
+Messages = Backbone.Collection.extend({
   model: Message
 });
-messages = new MessageList();
+User = Backbone.Model.extend({});
+Users = Backbone.Collection.extend({
+  model: User
+});
+users = new Users();
+messages = new Messages();
+
 firebase.child('messages').on('child_added', function(snapshot) {
   message = snapshot.val();
-  message.id = snapshot.name();
+  message._id = snapshot.name();
   messages.add(message);
 });
 firebase.child('messages').on('child_removed', function(snapshot) {
-  messages.remove(messages.where({id: snapshot.name()}));
-});
+  messages.remove(messages.where({_id: snapshot.name()}));
+});    
